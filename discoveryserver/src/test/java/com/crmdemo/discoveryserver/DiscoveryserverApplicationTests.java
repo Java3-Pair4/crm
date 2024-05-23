ef248daee85a7f13e981f69e176f5e4974dedfe2
CEMRE SEVAL VURAL <50405470+cemresvural@users.noreply.github.com>
2024-05-23 20:08:26 +0300

    23.05.2024,

---
+++ discoveryserver/src/test/java/com/crmdemo/discoveryserver/DiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crmdemo.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
